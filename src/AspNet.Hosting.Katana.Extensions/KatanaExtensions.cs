7e8d0badae54191201ea3375f07c0e49e5caf568
Kévin Chalet <kevinchalet@gmail.com>
2015-10-07 13:31:49 +0200

    Introduce a Katana/ASP.NET 5 bridge (app.UseKatana)

---
+++ src/AspNet.Hosting.Katana.Extensions/KatanaExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.DataProtection;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Internal;
+using Microsoft.Owin.Builder;
+using Microsoft.Owin.BuilderProperties;
+using Owin;
+
+namespace Microsoft.AspNet.Builder {
+    /// <summary>
+    /// Provides extension methods for <see cref="IApplicationBuilder"/>.
+    /// </summary>
+    public static class KatanaExtensions {
+        /// <summary>
+        /// Provides a Katana/ASP.NET 5 bridge allowing to register middleware designed for OWIN/Katana.
+        /// </summary>
+        /// <param name="app">The ASP.NET 5 application builder.</param>
+        /// <param name="configuration">
+        /// The delegate allowing to configure the OWIN/Katana
+        /// pipeline before adding it in the ASP.NET 5 application.
+        /// </param>
+        /// <returns>The ASP.NET 5 application builder.</returns>
+        public static IApplicationBuilder UseKatana(
+            [NotNull] this IApplicationBuilder app,
+            [NotNull] Action<IAppBuilder> configuration) {
+            return app.UseOwin(setup => setup(next => {
+                var builder = new AppBuilder();
+                var lifetime = app.ApplicationServices.GetService<IApplicationLifetime>();
+
+                var properties = new AppProperties(builder.Properties);
+                properties.AppName = app.ApplicationServices.GetApplicationUniqueIdentifier();
+                properties.OnAppDisposing = lifetime?.ApplicationStopping ?? CancellationToken.None;
+                properties.DefaultApp = next;
+
+                configuration(builder);
+
+                return builder.Build<Func<IDictionary<string, object>, Task>>();
+            }));
+        }
+    }
+}
---
+++ test/AspNet.Hosting.Katana.Extensions.Tests/KatanaExtensionsTests.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Hosting.Extensions for more information
+ * concerning the license and the contributors participating to this project.
+ */
+
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.TestHost;
+using Owin;
+using Xunit;
+
+namespace AspNet.Hosting.Katana.Extensions.Tests {
+    /// <summary>
+    /// This class contains tests for the Katana extensions.
+    /// </summary>
+    public class KatanaExtensionsTests {
+        /// <summary>
+        /// Tests that the Katana pipeline registered using
+        /// <see cref="KatanaExtensions.UseKatana"/>
+        /// is correctly registered in the ASP.NET 5 pipeline.
+        /// </summary>
+        [Fact]
+        public async Task KatanaPipelineCanBeAccessedFromAspNet() {
+            // Arrange
+            var server = TestServer.Create(app => app.UseKatana(builder => {
+                builder.Run(async context => {
+                    await context.Response.WriteAsync("Bob");
+                });
+            }));
+
+            var client = server.CreateClient();
+
+            // Act
+            var response = await client.GetStringAsync("/");
+
+            // Assert
+            Assert.Equal("Bob", response);
+        }
+
+        /// <summary>
+        /// Tests that the Katana pipeline registered using 
+        /// <see cref="KatanaExtensions.UseKatana"/> doesn't
+        /// prevent the rest of the ASP.NET 5 pipeline from being executed.
+        /// </summary>
+        [Fact]
+        public async Task KatanaPipelineIsNotTerminating() {
+            // Arrange
+            var server = TestServer.Create(app => {
+                app.UseKatana(builder => { });
+
+                app.Run(async context => {
+                    await context.Response.WriteAsync("Bob");
+                });
+            });
+
+            var client = server.CreateClient();
+
+            // Act
+            var response = await client.GetStringAsync("/");
+
+            // Assert
+            Assert.Equal("Bob", response);
+        }
+
+        /// <summary>
+        /// Tests that the Katana pipeline registered using 
+        /// <see cref="KatanaExtensions.UseKatana"/> stops
+        /// processing the request if next() is not invoked.
+        /// </summary>
+        [Fact]
+        public async Task KatanaPipelineCanStopRequestProcessing() {
+            // Arrange
+            var server = TestServer.Create(app => {
+                app.UseKatana(builder => builder.Run(async context => {
+                    await context.Response.WriteAsync("Alice");
+                }));
+
+                app.Run(async context => {
+                    await context.Response.WriteAsync("Bob");
+                });
+            });
+
+            var client = server.CreateClient();
+
+            // Act
+            var response = await client.GetStringAsync("/");
+
+            // Assert
+            Assert.Equal("Alice", response);
+        }
+    }
+}
